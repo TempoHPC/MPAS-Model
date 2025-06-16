5e29c52f31a1ed1883c6df8f18f89cdb75a292d8
isabeldefreitas <isabeldefreitasbarboza@gmail.com>
2025-06-16 15:42:24 -0300

    organização

--- docker/nvhpc_24.9/env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -v
-
-source /usr/share/modules/init/bash
-module use /opt/nvidia/hpc_sdk/modulefiles
-module load nvhpc-openmpi3/24.9
-source spack-0.23.1/share/spack/setup-env.sh
--- docker/nvhpc_24.9/singularity.md
+++
@@ -1,1 +0,0 @@
-singularity build mpas_8.sif docker-daemon://mpas:8.2.2
